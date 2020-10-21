d74465bbcec6552d02e9b752654a4394c5c65f80
Yifan Xia <yx2610@columbia.edu>
2020-10-21 17:05:06 +0000

    updated models

--- track/models.py
+++ track/models.py
@@ -2,9 +2,58 @@ from django.db import models
 from django.utils.translation import gettext as _
 
 class SquirrelDetail(models.Model):
-    name = models.CharField(
-            max_length=100,
-            help_text=_('Name of Squirrel'),
+    Latitude = models.DecimalField(
+        max_digits = 100,
+        decimal_places = 20,
+        help_text=_('Name of Squirrel'),
+    )    
+
+
+    Longtitude = models.DecimalField(
+        max_digits = 100,
+        decimal_places = 20
+        help_text=_('Name of Squirrel'),
+    )
+
+
+    Unique_Squirrel_ID = models.CharField(
+        max_length=15,
+        help_text=_('Unique ID of Squirrel'),
+    )
+
+
+    Age = models.CharField(
+        max_length=15,
+        help_text=_('Age of Squirrel'),
+    )
+
+
+    AM = 'AM'
+    PM = 'PM'
+
+
+    SHIFT_CHOICES = [
+        (AM,_('AM'),
+         PM,_('PM')),        
+    ]
+
+
+    shift = models.CharField(
+        max_length=5,
+        help_text=_('Shift of Sighting'),
+        chioces = SHIFT_CHOIcES
+        default = AM,
+    )       
+
+
+    date = models.DateField(
+        help_text = _('Date of Sighting'),        
     )
 
     
+    Squirrel_Image = mdoels.ImageField(
+        help_text = _('Image of Squirrel'),
+        blank = True,
+        upload_to = 'Profiles_Images'
+    )
+
