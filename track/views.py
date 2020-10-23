fc461b5ec05f4de08573e3f1029d06f720ef0fa8
Yifan Xia <yx2610@columbia.edu>
2020-10-23 01:45:46 +0000

    updated

--- track/views.py
+++ track/views.py
@@ -1,24 +1,42 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.db.models import Max, Min, Count, Avg
 from .models import Sighting
 from .forms import SightingForm
+from django.views.generic.edit import CreateView, DeleteView
 
 
 
 def map(request):
-    return render(request, 'track/map.html', {})
+    sightings = Sighting.objects.all()[:100]
+    context = {'sightings': sightings}
+    return render(request, 'track/map.html',context)
 
 
 #unique id
-def update_sighting(request,):
+def update_sighting(request,Unique_Squirrel_ID):
+    squirrel = get_object_or_404(Sighting, Unique_Squirrel_ID=Unique_Squirrel_ID)
+    form = SightingForm(request.POST or None, instance=squirrel)
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect(reverse('track:Sighting'))
     return render(request, 'track/update.html', {})
 
 
 #add
 def create_sighting(request):
-    
-    return render(request, 'track/add.html', {})
+    squirrel = Sighting()
+    if request.method == 'POST':
+        form = SightingForm(request.POST)
+        if form.is_valid():
+            squirrel = form.save()
+            return HttpResponseRedirect(reverse('track:Sighting'))
+    else:
+        form = SightingForm()        
+    return render(request, 'track/add.html', {'form': form,'squirrel':squirrel})
 
 
 #stats
@@ -28,14 +46,14 @@ def obtain_stats(request):
     avg_hectare_squirrel_num = squirrels.aggregate(Avg('Hectare Squirrel Number')).values()[0] 
     num_juve = len(squirrels.filter(age='Juvenile'))
     num_adult = len(squirrels.filter(age='Adult'))
-    fur_color = squirrels.values_list('values_list('Primary Fur Color').annotate(fur_count=Count('Primary Fur Color')).order_by('-fur_count')')[0]
-    content = {'total number of squrrels':total,
+    fur_color = squirrels.values_list('Primary Fur Color').annotate(fur_count=Count('Primary Fur Color')).order_by('-fur_count')
+    context = {'total number of squrrels':total,
                'average number of squirrels per hectare':avg_hectare_squirrel_num,
                'total number of juvenile':num_juve,
                'total number of adult':num_adult,
                'most common fur color':fur_color,
                }
-    return render(request, 'track/stats.html', content)
+    return render(request, 'track/stats.html', context)
 
 #sightings
 def list_sightings(request):
